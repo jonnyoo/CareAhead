7e9a43eef9fe9a2cd004ce2533b2cad684d62196
Jonathan Zhou <jonnyzgg@gmail.com>
2026-01-11 05:13:49 -0500

    fix bug

--- CareAhead/AppNavigation.swift
+++ CareAhead/AppNavigation.swift
@@ -1,7 +1,7 @@
 import Foundation
 import SwiftUI
+import Combine
 
-@MainActor
 final class AppNavigation: ObservableObject {
     /// Matches `ContentView` tab indices.
     @Published var selectedTab: Int = 0
