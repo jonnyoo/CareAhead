07ce916effe9a90bfe200058acbd5e6f1ddf6d7c
Your Name <your.email@example.com>
2026-01-11 02:46:19 -0500

    fixed ts

--- CareAhead/CareAheadApp.swift
+++ CareAhead/CareAheadApp.swift
@@ -12,7 +12,6 @@ import SwiftData
 struct CareAheadApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
             VitalSign.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
